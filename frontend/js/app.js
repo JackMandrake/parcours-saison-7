f5b5e97bf09c7e09da83b7e63a46514c4e0e75d0
JackMandrake <coignard.cyrille@hotmail.fr>
2020-07-16 11:46:05 +0200

    exo1-parcours7

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,4 +1,7 @@
 let app = {
+
+    apiRootUrl: 'http://localhost:8080',
+
     init: function() {
         console.log('app.init()');
 
@@ -10,26 +13,79 @@ let app = {
     },
     addAllEventListeners: function() {
         // On récupère l'élément <select> des jeux vidéo
-        
+        selectElm = document.getElementById('videogameId');
         // On ajoute l'écouteur pour l'event "change", et on l'attache à la méthode app.handleVideogameSelected
+        selectElm.addEventListener('change', app.handleVideogameSelected)
 
         // On récupère le bouton pour ajouter un jeu vidéo
         let addVideogameButtonElement = document.getElementById('btnAddVideogame');
         // On ajoute l'écouteur pour l'event "click"
         addVideogameButtonElement.addEventListener('click', app.handleClickToAddVideogame);
-        
+        console.log(selectElm);
         // TODO
     },
+
     handleVideogameSelected: function(evt) {
+        console.log('handleVideogameSelected')
         // Récupérer la valeur du <select> (id du videogame)
-
+        let videoGamesSelected = evt.currentTarget;
+        let gameSelected = videoGamesSelected.value;
+        console.log('je suis le jeu video : ' +gameSelected);
         // Vider le contenu de div#review
-
+        let divReview = document.getElementById('review').textContent = '';
         // charger les données pour ce videogame
-            // Dupliquer la template #reviewTemplate et personnaliser son contenu avec les données
+        let fetchOptions = {
+            method: 'GET',
+            mode: 'cors',
+            cache: 'no-cache'
+        };
 
+        // On déclanche la requête HTTP  videogames/{id}/reviews
+        fetch(app.apiRootUrl + '/videogames/' + gameSelected + '/reviews', fetchOptions)
+        .then(
+            function(response) {
+            return response.json();
+        })
+        .then(function(singleGame) {
+
+            console.log('Reviews de mon jeu via le singleGame : ', singleGame);
+        
             // Ajouter dans le DOM
+
+            app.createNewGameIntoDOM(singleGame);
+
+            console.log(singleGame);
+        });               
+            
     },
+
+    createNewGameIntoDOM: function(singleGame, gameSelected) {
+
+        // Dupliquer la template #reviewTemplate et personnaliser son contenu avec les données
+
+        let newVideoTemplateElement = document.getElementById('reviewTemplate');
+        let newVideoElement = newVideoTemplateElement.content.cloneNode(true).querySelector('.reviewContainer');
+        
+        console.log(singleGame[0].author);
+
+                
+        newVideoElement.querySelector('.reviewAuthor').textContent = singleGame[0].author;
+        newVideoElement.querySelector('.reviewPublication').textContent = singleGame[0].publication_date;
+        newVideoElement.querySelector('.reviewTitle').textContent = singleGame[0].title;
+        newVideoElement.querySelector('.reviewDisplay').textContent = singleGame[0].display_note;
+        newVideoElement.querySelector('.reviewGameplay').textContent = singleGame[0].gameplay_note;
+        newVideoElement.querySelector('.reviewScenario').textContent = singleGame[0].scenario_note;
+        newVideoElement.querySelector('.reviewLifetime').textContent = singleGame[0].lifetime_note;
+        newVideoElement.querySelector('.reviewText').textContent = singleGame[0].text;
+
+       
+
+
+        let videogameContainer = document.querySelector('#review');
+        videogameContainer.prepend(newVideoElement);
+
+    },
+
     handleClickToAddVideogame: function(evt) {
         // https://getbootstrap.com/docs/4.4/components/modal/#modalshow
         // jQuery obligatoire ici
